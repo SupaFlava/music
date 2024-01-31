59a84ae64de00e2c0163ef3c69ad29b8758483eb
Rami Mhazres <superflava8@gmail.com>
2024-01-31 15:53:53 +0100

    User Registration

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -101,6 +101,7 @@
 </template>
 
 <script>
+import firebase from '@/includes/firebase'
 export default {
   name: 'registerForm',
 
@@ -125,14 +126,17 @@ export default {
     }
   },
   methods: {
-    register(values) {
+    async register(values) {
       this.reg_show_alert = true
       this.reg_in_submission = true
       this.reg_alert_variant = 'bg-blue-500'
       this.reg_alert_message = 'Please wait! Your account is being created.'
+
+      const userCred = await firebase
+        .auth()
+        .createUseWithEmailAndPassword(values.email, values.password)
       this.reg_alert_variant = 'bg-green-500'
       this.reg_alert_message = 'sucess! your account has been created.'
-      console.log(value)
     }
   }
 }
