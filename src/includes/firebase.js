51b74d4022bbdb13503b5a986b97527fc01cfaee
Rami Mhazres <superflava8@gmail.com>
2024-01-31 15:45:10 +0100

    setting up firebase sdk

---
+++ src/includes/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app'
+
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDq2w59ZBhrhwLDKIoOYsQjFNVpPkhQh3A',
+  authDomain: 'music-c3a5e.firebaseapp.com',
+  projectId: 'music-c3a5e',
+  storageBucket: 'music-c3a5e.appspot.com',
+  appId: '1:750230533789:web:289610b7efbe1b257f4674',
+  measurementId: 'G-DQQ0YBBB84'
+}
+
+export default initializeApp(firebaseConfig)
