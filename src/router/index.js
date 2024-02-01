aeaec39e68bd8303b9697093728cf497300fe046
Rami Mhazres <superflava8@gmail.com>
2024-02-01 15:58:32 +0100

    Meta Fields & Guards

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/HomeView.vue'
 import About from '@/views/AboutView.vue'
 import Manage from '@/views/ManageView.vue'
+import useUserStore from '@/stores/user'
 const routes = [
   {
     name: 'home',
@@ -16,6 +17,9 @@ const routes = [
     beforeEnter: (to, from, next) => {
       console.log('Manage Route Guard')
       next()
+    },
+    meta: {
+      requiresAuth: true
     }
   },
   {
@@ -28,9 +32,21 @@ const routes = [
   }
 ]
 const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
   linkExactActiveClass: 'text-yellow-500'
+})
+router.beforeEach((to, from, next) => {
+  if (!to.meta.requiresAuth) {
+    next()
+    return
+  }
+  const store = useUserStore()
+  if (store.userLoggedIn) {
+    next()
+  } else {
+    next({ name: 'home' })
+  }
 })
 
 export default router
