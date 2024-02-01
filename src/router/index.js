4826a7e037fa4574000e08342015e609d0d0cd66
Rami Mhazres <superflava8@gmail.com>
2024-02-01 14:54:22 +0100

    custom links and styling

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '@/views/HomeView.vue'
 import About from '@/views/AboutView.vue'
+import Manage from '@/views/ManageView.vue'
 
 const routes = [
   {
@@ -10,11 +11,13 @@ const routes = [
   {
     path: '/about',
     component: About
-  }
+  },
+  { path: '/manage', component: Manage }
 ]
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes,
+  linkExactActiveClass: 'text-yellow-500'
 })
 
 export default router
