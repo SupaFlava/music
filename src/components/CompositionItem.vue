170e76930828bf783f789ceb4a56348bf8fa1ce2
Rami Mhazres <superflava8@gmail.com>
2024-02-03 11:03:24 +0100

    Prop Validation for Songs

--- src/components/CompositionItem.vue
+++ src/components/CompositionItem.vue
@@ -35,6 +35,12 @@
 </template>
 <script>
 export default {
-  name: 'CompositionItem'
+  name: 'CompositionItem',
+  props: {
+    song: {
+      type: Object,
+      required: true
+    }
+  }
 }
 </script>
