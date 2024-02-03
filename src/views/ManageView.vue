32db8abb9bd2704a769da4d59bba5d793fbc2491
Rami Mhazres <superflava8@gmail.com>
2024-02-03 11:55:02 +0100

    Deleting a Song

--- src/views/ManageView.vue
+++ src/views/ManageView.vue
@@ -19,6 +19,7 @@
               :song="song"
               :updateSong="updateSong"
               :index="i"
+              :removeSong="removeSong"
             />
           </div>
         </div>
@@ -57,6 +58,9 @@ export default {
     updateSong(i, values) {
       this.songs[i].modified_name = values.modified_name
       this.songs[i].genre = values.genre
+    },
+    removeSong(i) {
+      this.songs.splice(i, 1)
     }
   }
 
