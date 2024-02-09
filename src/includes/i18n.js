3088f214941898ca1b1ef61a8abd4e12bd664685
Rami Mhazres <superflava8@gmail.com>
2024-02-09 11:19:24 +0100

    Formating and Pluralization

--- src/includes/i18n.js
+++ src/includes/i18n.js
@@ -1,10 +1,12 @@
 import { createI18n } from 'vue-i18n'
 import en from '@/locales/en.json'
+import nl from '@/locales/nl.json'
 
 export default createI18n({
   locale: 'en',
   fallbackLocale: 'en',
   messages: {
-    en
+    en,
+    nl
   }
 })
