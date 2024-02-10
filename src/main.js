eabb82dea11e8e01d10409af0754f3686625b54b
Rami Mhazres <superflava8@gmail.com>
2024-02-10 10:46:28 +0100

    Auto-Registering Global Components

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import App from './App.vue'
 import router from './router'
 import VeeValidatePlugin from './includes/validation'
 import { auth } from './includes/firebase'
+import GlobalComponents from './includes/_globals'
 
 import Icon from './directives/icon'
 import i18n from './includes/i18n'
@@ -26,6 +27,7 @@ auth.onAuthStateChanged(() => {
     app.use(router)
     app.use(VeeValidatePlugin)
     app.use(i18n)
+    app.use(GlobalComponents)
     app.directive('icon', Icon)
     app.mount('#app')
   }
